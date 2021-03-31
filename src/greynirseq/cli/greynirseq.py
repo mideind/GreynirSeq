5a17853a7a8dfae529bd70d72abd229f837a20d3
Vésteinn Snæbjarnarson <vesteinnsnaebjarnarson@gmail.com>
2021-03-31 17:25:02 +0000

    Check for long sentences in CLI

--- src/greynirseq/cli/greynirseq.py
+++ src/greynirseq/cli/greynirseq.py
@@ -8,12 +8,13 @@ import tqdm
 
 
 class GreynirSeqIO:
-    def __init__(self, input, output, device, batch_size, show_progress):
+    def __init__(self, input, output, device, batch_size, show_progress, max_input_words_split):
         self.input = input
         self.output = output
         self.device = device
         self.batch_size = batch_size
         self.show_progress = show_progress
+        self.max_input_length = max_input_words_split
         self.model = self.build_model()
 
     def build_model(self):
@@ -21,6 +22,11 @@ class GreynirSeqIO:
 
     def infer(self, batch):
         raise NotImplementedError
+
+    def handle_batch(self, batch):
+        results = self.infer(batch)
+        for result in results:
+            self.output.write(result + "\n")
 
     def run(self):
         batch = []
@@ -31,19 +37,38 @@ class GreynirSeqIO:
         for line in input_iterable:
             if not line:
                 # If input is empty write empty line to preserve order
+                if batch:
+                    self.handle_batch(batch)
+                    batch = []
                 self.output.write("\n")
+                continue
+
+            split_line = line.split()
+            split_line_length = len(split_line)
+            if split_line_length > self.max_input_length:
+                if batch:
+                    self.handle_batch(batch)
+                    batch = []
+
+                ranges = range(0, split_line_length, self.max_input_length)
+                temp_batch = []
+                for i in ranges:
+                    temp_batch.append(" ".join(split_line[i : i + self.max_input_length]))
+
+                sub_batches = range(0, len(temp_batch), self.batch_size)
+                results = []
+                for i in sub_batches:
+                    inference = self.infer(temp_batch[i : i + self.batch_size])
+                    results += inference
+                self.output.write(" ".join(results) + "\n")
                 continue
 
             batch.append(line)
             if len(batch) == self.batch_size:
-                results = self.infer(batch)
-                for result in results:
-                    self.output.write(result + "\n")
+                self.handle_batch(batch)
                 batch = []
         if batch:
-            results = self.infer(batch)
-            for result in results:
-                self.output.write(result + "\n")
+            self.handle_batch(batch)
 
 
 class NER(GreynirSeqIO):
@@ -87,6 +112,13 @@ def main():
     parser.add_argument("--device", action="store", dest="device", type=str, default="cpu")
     parser.add_argument("--batch-size", action="store", dest="bsz", type=int, default=1)
     parser.add_argument("--progressbar", action="store_true", dest="progress", default=False)
+    parser.add_argument(
+        "--max-input-words-split",
+        action="store",
+        dest="max_input_words_split",
+        default=250,
+        help="Splits input-string before inference if spaces exceed this amount, re-joins afterwards.",
+    )
 
     args = parser.parse_args()
     if args.command.lower() == "ner":
@@ -95,7 +127,7 @@ def main():
     elif args.command.lower() == "pos":
         command = POS
 
-    handler = command(args.input, args.output, args.device, args.bsz, args.progress)
+    handler = command(args.input, args.output, args.device, args.bsz, args.progress, args.max_input_words_split)
     handler.run()
 
 
