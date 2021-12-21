4a38f4c33f65617c1953d65094f4b012599739e9
Haukur Páll <haukurpalljonsson@gmail.com>
2021-12-21 12:52:28 +0000

    Only supporting building cython on linux.

--- build.py
+++ build.py
@@ -4,19 +4,27 @@ This module is called by poetry during the installation process."""
 import os
 import sys
 
+do_build = False
 # See if Cython is installed
 try:
     from Cython.Build import cythonize
+
+    # Cython is installed.
+    # We only support building for linux.
+    if sys.platform.startswith("linux"):
+        do_build = True
 # Do nothing if Cython is not available
 except ImportError:
+    pass
+
+if not do_build:
     # Got to provide this function. Otherwise, poetry will fail
-    print("Cython is not installed. Skipping build.", file=sys.stderr)
+    print("Cython is not or installed or different platform from linux. Skipping build.", file=sys.stderr)
 
     def build(setup_kwargs):
         pass
 
 
-# Cython is installed. Compile
 else:
     from distutils.command.build_ext import build_ext
 
